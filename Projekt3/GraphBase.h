f007b1a8cf1b1f39a55bf789c0466d27ad1e8035
wiecek1873 <wiecek1873@gmail.com>
2020-04-27 18:25:22 +0200

    +FillGraph

--- Projekt3/GraphBase.h
+++ Projekt3/GraphBase.h
@@ -2,6 +2,7 @@
 #include <vector>
 #include <algorithm>
 #include <climits>
+#include <iostream>
 
 class GraphBase
 {
@@ -11,5 +12,24 @@ public:
 	virtual void deleteEdge(int u, int v) = 0;
 	virtual std::vector<int> neighbours(int u) = 0;
 	virtual int weight(int u, int v) = 0;
+
+	void fillRandom(float density)
+	{
+		for (int i = 0; i < _numberOfVertices; ++i)
+		{
+			for (int j = 0; j < _numberOfVertices; ++j)
+			{
+				if (i != j)
+					addEdge(i, j, rand() % 100);
+			}
+		}
+		int numberOfEdges = (int)(1 * _numberOfVertices * (_numberOfVertices - 1));
+		int numberOfEdgesToDelete = numberOfEdges - (int)(density * _numberOfVertices * (_numberOfVertices - 1));
+		while (numberOfEdgesToDelete > 0)
+		{
+			deleteEdge(rand() % _numberOfVertices, rand() % _numberOfVertices);
+			--numberOfEdgesToDelete;
+		}
+	}
 };
 
